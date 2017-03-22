921c98f23ceee7a2a3f972b75b8b0e4269b26430
xKxAxKx <pekepooooong@gmail.com>
2017-03-22 11:34:26 +0900

    first commit

---
+++ src/app/mock-heroes.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.HEROES = [
+    { id: 11, name: 'Uzuki' },
+    { id: 12, name: 'Rin' },
+    { id: 13, name: 'Mio' },
+    { id: 14, name: 'Chieri' },
+    { id: 15, name: 'Kanako' },
+    { id: 16, name: 'Anzu' },
+    { id: 17, name: 'Minami' },
+    { id: 18, name: 'Miria' },
+    { id: 19, name: 'Ranko' },
+    { id: 20, name: 'Kirari' }
+];
+//# sourceMappingURL=mock-heroes.js.map
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Uzuki' },
+  { id: 12, name: 'Rin' },
+  { id: 13, name: 'Mio' },
+  { id: 14, name: 'Chieri' },
+  { id: 15, name: 'Kanako' },
+  { id: 16, name: 'Anzu' },
+  { id: 17, name: 'Minami' },
+  { id: 18, name: 'Miria' },
+  { id: 19, name: 'Ranko' },
+  { id: 20, name: 'Kirari' }
+];
